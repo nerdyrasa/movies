56ac1d716a9ad6656282fe9c2c583570312d2ec3
Roz Isaacson <rozisaacson@users.noreply.github.com>
2015-10-19 14:13:42 -0500

    refactor

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,16 +1,20 @@
+import sys
 import fresh_tomatoes
 import json
 import media
 
-# open json file
-with open("movie_list.json") as movie_file:
-    movie_data = json.load(movie_file)
 
-movie_list = []
+def main():
+    """Main entry point for app"""
+    # open json file
+    with open("movie_list.json") as movie_file:
+        movie_data = json.load(movie_file)
 
-# iterate through the movie_data and create instance of Movie object
-for movie in movie_data:
-    movie_list.append(media.Movie(movie))
+    # iterate through the movie_data and create instance of Movie object
+    movie_list = [media.Movie(movie) for movie in movie_data]
 
-# create the html page
-fresh_tomatoes.open_movies_page(movie_list)
+    # create the html page
+    fresh_tomatoes.open_movies_page(movie_list)
+
+if __name__ == '__main__':
+    sys.exit(main())
